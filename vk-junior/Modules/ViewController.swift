48cdad01e8e1d0a24c49c5836d43f73ab3e979dd
stoicist <art.igberdin@gmail.com>
2021-09-21 17:18:09 +0300

    Initialize project

---
+++ vk-junior/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vk-junior
+//
+//  Created by Artur Igberdin on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
